bd273ae31a60c8aa6d86cea9083ac83e2fb13dbe
Gokul T P <tp.gokul@gmail.com>
2019-08-20 02:29:12 +0530

    added flag checks and version menu in amin

--- cmd/hashqd/main.go
+++ cmd/hashqd/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/gokultp/hashqd/internal/connection"
 	"github.com/gokultp/hashqd/internal/queue"
+	"github.com/gokultp/hashqd/internal/version"
 )
 
 func main() {
+	versionFound := false
+	configPath := "/etc/hashqd/hashqd.conf"
+	flag.BoolVar(&versionFound, "version", false, "prints version details")
+	flag.StringVar(&configPath, "config-path", "/etc/hashqd/hashqd.conf", "path of the config file")
+	flag.Parse()
+	if versionFound {
+		version.PrintVersion()
+		return
+	}
 	queue.Init()
 	err := connection.Listen("9000")
 	if err != nil {
